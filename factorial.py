63ed102fbac2faa0e1b1d0caa68f5390617f03d1
sachin1437 <136810648+sachin1437@users.noreply.github.com>
2025-09-04 11:48:28 +0530

    Update and rename first.py to factorial.py

--- factorial.py
+++ factorial.py
@@ -4,4 +4,5 @@ n=int(input("Enter a number: "))
 fact=1
 for i in range(1,n+1):
     fact=fact*i  # here fact is being multiplied by every value of i in the range of 1 to n and if i write print(fact) here it will print every value of fact which is being solved
+
 print(fact)  # here i have written the print statement outside the loop to avoid printing every value of fact which is being solved 
